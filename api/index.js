bdf69dd08abee01e2167e4818685dd3dc49fa030
Fake-404 <120786742+Fake-404@users.noreply.github.com>
2024-12-17 17:59:32 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT||3000;
+
+//Routes
+import test from './routers/test.js';
+import InstagramPost from './routers/instagramPost.js';
+
+app.use('/', test);
+app.use('/igpost', InstagramPost);
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`)
+})
