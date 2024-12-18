2f5278288d55776bc9dc39b5fc2ffec6d35c4556
Fake-404 <120786742+Fake-404@users.noreply.github.com>
2024-12-18 19:00:05 +0530

    Update instgramPost.js

--- Instagram/instgramPost.js
+++ Instagram/instgramPost.js
@@ -248,7 +248,7 @@ ctx.fillText(shareCount, shareX + shareSize + 3, shareY + 16);
 
 
     const saveImage = await loadImage(savePng(this.saved)); // Replace with the path to your heart PNG image
-const saveX = 300; // X position of the heart image
+const saveX = 350; // X position of the heart image
 const saveY = footerY + 14; // Y position of the heart image
 const saveSize = 20; // Size of the heart image
 
